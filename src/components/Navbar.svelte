8f2be8afeae95dc419b4fca556ab4833a6df1b33
Caroline SENES <caro67200@gmail.com>
2022-02-28 15:57:55 +0100

    add navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,19 @@
+<script>
+    // your script goes here
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+
+<nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <h1>Budget Calculator</h1>
+      <form class="d-flex">
+        <button class="btn btn-outline-success me-2" type="button">
+          <i class="bi bi-plus-square" />
+		  Ajouter une dépense
+        </button>
+      </form>
+    </div>
+</nav>
---
+++ src/components/Title.svelte
@@ -0,0 +1,13 @@
+<script>
+    //on exporte pour pouvoir modifier le titre de l'extérieur du component
+    //le titre par défaut sera "Dépense anonyme"
+    export let title = "Dépense anonyme"
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+
+<div class="">
+    <h2>{title}</h2>
+</div>
