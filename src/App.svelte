5943fb67e6c8a87d3342efb9299ca80f2501d14e
SENES <caro67200@gmail.com>
2022-03-04 11:42:25 +0100

    add footer

--- src/App.svelte
+++ src/App.svelte
@@ -131,6 +131,11 @@
   <Totals {total} />
   <ExpensesList {expenses} />
 </main>
+<footer class="footer mt-auto py-3">
+  <div class="container text-center">
+    <p>Développé avec Svelte par <a href="mailto:senes.caroline@orange.fr">Caroline SENES</a> et conforme aux normes du <a href="https://www.w3.org/Status">W3C</a>.</p>
+  </div>
+</footer>
 
 
 <style>
